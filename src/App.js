9960c27e5a3a405a3293c258cdab0587392f0836
SP UDAY KIRAN REDDY <udaykiranreddysp25@gmail.com>
2024-10-14 10:25:05 +0530

    enter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [range, setRange] = useState(0);
+  const date = new Date("september 16 2024");
+  date.setDate(date.getDate() + count);
+  function handleClick() {
+    setCount(0);
+  }
+  return (
+    <>
+      <div className="count">
+        <input
+          type="range"
+          min={0}
+          max={10}
+          value={range}
+          onChange={(e) => setRange(Number(e.target.value))}
+        />
+        <span>step : {range}</span>
+      </div>
+      <div className="count">
+        <button onClick={() => setCount(count - range)}>-</button>
+        <input
+          type="number"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={() => setCount(count + range)}>+</button>
+      </div>
+      <div className="dateChange">
+        <p>
+          <span>
+            {count === 0
+              ? "Today is "
+              : count > 0
+              ? `${count} days from today is `
+              : `${Math.abs(count)} days ago was `}
+          </span>
+          <span>{date.toDateString()}</span>
+        </p>
+      </div>
+      {count !== 0 || range !== 1 ? (
+        <div className="count">
+          <button onClick={handleClick}>Reset</button>
+        </div>
+      ) : null}
+    </>
+  );
+}
